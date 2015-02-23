4e06d5ead0b2d238ac81b330406491588e2e20b0
Dan O'Huiginn <daniel@ohuiginn.net>
2015-02-23 20:24:23 +0100

    fix DNV scraper

--- scrapers/dnv.py
+++ scrapers/dnv.py
@@ -1,3 +1,9 @@
+import scrapekit
+import copy
+import dataset
+import datetime
+import base
+import logging
 
 
 search_terms = (
@@ -25,29 +31,48 @@ search_terms = (
     "gabon",
 )
 
-import scrapekit
-import copy
-import dataset
-import datetime
-import base
 
 scraper = scrapekit.Scraper('dnv')
 
 
+
+table = base.db['rigs']
+
+
+
+
+
+def get_labelled(html, label):
+        span = html.cssselect('#' + label)
+        if not span:
+            return ''
+        return span[0].text_content().strip()
+        
+
 @scraper.task
-def vessel_details(vesselid, vesseldetails={}):
+def vessel_details(vesselid, vesseldetails):
+    logging.info('getting details of %s' % vesselid)
     url = "https://exchange.dnv.com/Exchange/Main.aspx?EXTool=Vessel&VesselID=%s" % vesselid
     html = scraper.get(url).html()
-    # XXX writeme
-    return html
+
+    span_labels = {
+
+        'docholder': 'ucMainControl_ToolContainer__ctl1_tabControl__ctl1_mDocHolderLink',
+        'yard': 'ucMainControl_ToolContainer__ctl1_tabControl__ctl1_mYard',
+        'shape': 'ucMainControl_ToolContainer__ctl1_tabControl__ctl1_mShape',
+    }
+    # span_labels omits data we already get from the js feed, namely:
+    # callsign, capacity, classification, competitive_rig, country, current_water_depth, delivery year, derrick, detail_uri, drawworks, drilling_depth, field_operator, flag, flagcode, hasdnvservices, historicalnames, hitfields, hitfieldsdescription, homeport, id, imono, indnvclass, inglclass, location_field, mainclass, manager, managerid, managername, mud pumps, name, nonclassservices, nonclassservicevesselid, officialno, operating_status, operator, owner, ownerid, ownername, rated_water_depth, region, register, registercode, rig design, rig_design, rig_id, rig_type, rotary table, ruleset, ship_type, shipyard, site, source_uri, top drive, vesseltype
 
 
+    for (k,v) in span_labels.items():
+        if not vesseldetails.get(k, None):
+            vesseldetails[k] = get_labelled(html, v)
+    table.upsert(vesseldetails, ['id'])
+    return vesseldetails
 
 @scraper.task
 def scrape_index(search_term):
-
-    table = base.db['rigs']
-
     basic_data = {
         # copypasta for base.Scraper.setup()
 
@@ -69,7 +94,16 @@ def scrape_index(search_term):
             entry[k.lower()] = str(v)
         print('adding one item')
         table.upsert(entry, ['site', 'name', 'imono'])
+        vessel_details.queue(entry['id'], entry)
     return
 
-
     
+@scraper.task
+def scrape_all_indexes():
+    for term in search_terms:
+        logging.info('scraping js for %s' % term)
+        scrape_index(term)
+
+
+if __name__ == '__main__':
+    scrape_all_indexes()
